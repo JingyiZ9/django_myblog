7b106b00a0a54ab200b0884afbed07ab496ae2a3
JingyiZ9 <jingyiz9@illinois.edu>
2019-04-09 18:43:36 -0500

    update the model

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 
 # Create your views here.
@@ -7,4 +8,14 @@ from django.http import HttpResponse
 # receive a request as a parameter
 # returns a response as a result
 def home(request):
-    return HttpResponse('Hello, World!')
+    #list all the objects in the Board
+    boards = Board.objects.all()
+    #create an empty list to store the board names
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
